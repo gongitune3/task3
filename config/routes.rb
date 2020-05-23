c2a2afdc48c1117ac1d5ef6a179a1dccedfcce89
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-05-23 18:20:02 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :books
-  resources :users, only: [:show, :edit, :update,]
-  get "/" => 'users#top'
+  resources :users, only: [:show, :index, :edit, :update,]
   get "books/about" => 'books#about'
   get "books/index" => 'books#index'
-  root 'users#index'
+  get "/" => 'users#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
